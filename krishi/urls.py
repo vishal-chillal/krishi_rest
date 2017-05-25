74252378f04accab2c5a87ac1be5092ccc1715e1
vishal-k3gster <vishal.k3gster@gmail.com>
2017-05-25 14:57:37 +0000

    login and signin POST is working with csrf token.

--- krishi/urls.py
+++ krishi/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-
+from django.template.loader import *
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^signin/$', views.signin, name='signin'),
+    url(r'^login/$', views.login, name='login')
 ]
--- krishi/views.py
+++ krishi/views.py
@@ -1,5 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from krishi.forms import LoginForm
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the krishi index.")
+
+
+def login(request):
+    username = "not loged in"
+    if request.method == "POST":
+        MyLoginForm = LoginForm(request.POST)
+
+        # username = MyLoginForm.cleaned_data['username']
+        print request.body.split('&')[1]
+        if MyLoginForm.is_valid():
+            username = MyLoginForm.cleaned_data['username']
+    else:
+        MyLoginForm = LoginForm()
+    return render(request, 'loggedin.html', {"username": username})
+    # return render(request, 'login.html')
+
+
+def signin(request):
+    # return HttpResponseRedirect('login.html')
+    return render(request, 'login.html')
