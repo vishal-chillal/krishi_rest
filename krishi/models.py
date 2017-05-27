bde9b0f8efc496b6878ef00b862fb3adce98b82d
vishal-k3gster <vishal.k3gster@gmail.com>
2017-05-27 19:01:52 +0000

    applied some css and started new model.

--- krishi/models.py
+++ krishi/models.py
@@ -4,7 +4,13 @@ from django.db import models
 
 
 class UserInfo(models.Model):
+    """class for user login """
     user = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+
+
+class Event(models.Model):
+    """docstring for Event"""
+    
 
 # Create your models here.
--- krishi/views.py
+++ krishi/views.py
@@ -10,12 +10,21 @@ def index(request):
 
 def login(request):
     username = "not loged in"
+    resp = {}
     if request.method == "POST":
         data = clean_json(request.body)
         data = json.loads(data)
         if validate_login(data):
             username = data['username']
-    return render(request, 'loggedin.html', {"username": username})
+            resp["status"] = 201
+            resp["message"] = "logged in"
+            resp["username"] = username
+    else:
+        username = "not 123"
+        resp["status"] = 403
+        resp["message"] = "Invalid"
+        resp["username"] = username
+    return render(request, 'loggedin.html', resp)
 
 
 def signin(request):
